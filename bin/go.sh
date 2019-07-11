9aa322ce24fb3b665b6da4c185e848427d874ed2
Your Name <you@example.com>
2019-07-11 10:47:28 +0800

    update proxy port

--- bin/go.sh
+++ bin/go.sh
@@ -1,3 +1,6 @@
-./shadowsocks2 -c 'ss://AES-128-CFB:fengmushu@183.2.217.51:8848' \
+
+IPADDR=`ip addr show dev eno1 | grep "inet " | awk -F/  '{print $1}' | awk '{print $2}'`
+
+./shadowsocks2 -c "ss://AES-128-CFB:fengmushu@${IPADDR}:8848" \
 	    -verbose -socks :1088 -u -udptun :8053=8.8.8.8:53,:8054=8.8.4.4:53 \
 	                                 -tcptun :8053=8.8.8.8:53,:8054=8.8.4.4:53
